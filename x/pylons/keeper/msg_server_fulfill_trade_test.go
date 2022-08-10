9a477c267c4dfbb11e7dfc29866b8a185634e117
ThanhNhan <pc@EricNguyen-2.local>
2022-08-10 23:47:31 +0700

    update test fulfill trade

--- x/pylons/keeper/msg_server_fulfill_trade_test.go
+++ x/pylons/keeper/msg_server_fulfill_trade_test.go
@@ -12,56 +12,24 @@ import (
 	"github.com/Pylons-tech/pylons/x/pylons/types"
 )
 
-func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple() {
-	k := suite.k
-	ctx := suite.ctx
-	require := suite.Require()
-
-	wctx := sdk.WrapSDKContext(ctx)
-	srv := keeper.NewMsgServerImpl(k)
-
-	creatorA := types.GenTestBech32FromString("creatorA")
-	creatorB := types.GenTestBech32FromString("creatorB")
-
-	for i := 0; i < 5; i++ {
-		msgCreate := &types.MsgCreateTrade{
-			Creator:     creatorA,
-			CoinInputs:  nil,
-			ItemInputs:  nil,
-			CoinOutputs: nil,
-			ItemOutputs: nil,
-			ExtraInfo:   "extrainfo",
-		}
-
-		respCreate, err := srv.CreateTrade(wctx, msgCreate)
-		require.NoError(err)
-		require.Equal(i, int(respCreate.Id))
-
-		msgFulfill := &types.MsgFulfillTrade{
-			Creator:         creatorB,
-			Id:              respCreate.Id,
-			CoinInputsIndex: 0,
-			Items:           nil,
-		}
-
-		_, err = srv.FulfillTrade(wctx, msgFulfill)
-		require.NoError(err)
-	}
-}
-
 func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 	k := suite.k
 	ctx := suite.ctx
 	require := suite.Require()
+	bk := suite.bankKeeper
 
 	wctx := sdk.WrapSDKContext(ctx)
 	srv := keeper.NewMsgServerImpl(k)
 
 	privKey := ed25519.GenPrivKey()
-	creator := types.GenTestBech32FromString("creator")
+	creatorA := types.GenTestBech32FromString("creator1")
+	creatorB := types.GenTestBech32FromString("creator2")
+
+	trashStr := types.GenTestBech32FromString("trash")
+	trashAddress := sdk.MustAccAddressFromBech32(trashStr)
 
 	msgCreate := &types.MsgCreateTrade{
-		Creator:     creator,
+		Creator:     creatorA,
 		CoinInputs:  nil,
 		ItemInputs:  nil,
 		CoinOutputs: nil,
@@ -69,22 +37,25 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		ExtraInfo:   "extrainfo",
 	}
 	for index, tc := range []struct {
-		desc                         string
-		msgFulfill                   types.MsgFulfillTrade
-		updateIdMsgFulfill           bool
-		msgCreate                    types.MsgCreateTrade
-		updateCoinInputsMsgCreate    bool
-		updatePaymentInfosForProcess bool
-		updateItemOutputsMsgCreate   bool
-		setItem                      bool
-		setUpPayFee                  bool
-		tradeableOfItem              bool
-		valid                        bool
+		desc                            string
+		msgFulfill                      types.MsgFulfillTrade
+		updateIdMsgFulfill              bool
+		msgCreate                       types.MsgCreateTrade
+		updateCoinInputsMsgCreate       bool
+		updatePaymentInfosForProcess    bool
+		updateItemOutputsMsgCreate      bool
+		setItem                         bool
+		tradeableOfItem                 bool
+		setUpPayFeeForTradeFulFill      bool
+		setUpSendCoinsToCookBookCreator bool
+		setUpPayFeeForTradeCreate       bool
+		setTradePercentage              bool
+		valid                           bool
 	}{
 		{
 			desc: "Trade does not exist",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items:           nil,
@@ -101,7 +72,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Invalid coinInputs index",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 2,
 				Items:           nil,
@@ -118,7 +89,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Invalid PaymentInfo",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items:           nil,
@@ -145,7 +116,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Process PaymentInfo error",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items:           nil,
@@ -170,7 +141,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Not enough balance to pay for trade coinInputs",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items:           nil,
@@ -187,7 +158,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Error match ItemInputs for trade",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items: []types.ItemRef{
@@ -209,7 +180,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Cannot be traded with item and cookbook id",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items: []types.ItemRef{
@@ -231,7 +202,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "Cannot use CoinOutputs to pay for the items provided",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items: []types.ItemRef{
@@ -253,7 +224,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 		{
 			desc: "CoinInputs not sufficient to pay transfer fees",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items:           nil,
@@ -268,9 +239,9 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 			valid:                        false,
 		},
 		{
-			desc: "Not enough fees for paid Item Match",
+			desc: "Insufficient fees from Trade FulFill to pay for inputChain",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items: []types.ItemRef{
@@ -287,13 +258,56 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 			updateItemOutputsMsgCreate:   false,
 			setItem:                      false,
 			tradeableOfItem:              false,
-			setUpPayFee:                  true,
+			setUpPayFeeForTradeFulFill:   true,
+			setTradePercentage:           true,
+			valid:                        false,
+		},
+		{
+			desc: "Insufficient fees from Trade FulFill to send coins to cookbook's creator",
+			msgFulfill: types.MsgFulfillTrade{
+				Creator:         creatorB,
+				Id:              0,
+				CoinInputsIndex: 0,
+				Items: []types.ItemRef{
+					{},
+					{},
+				},
+			},
+			updateIdMsgFulfill:              false,
+			msgCreate:                       *msgCreate,
+			updateCoinInputsMsgCreate:       false,
+			updatePaymentInfosForProcess:    false,
+			updateItemOutputsMsgCreate:      false,
+			tradeableOfItem:                 false,
+			setUpSendCoinsToCookBookCreator: true,
+			setTradePercentage:              true,
+			valid:                           false,
+		},
+		{
+			desc: "Insufficient fees from Trade Creator to pay for outputChain",
+			msgFulfill: types.MsgFulfillTrade{
+				Creator:         creatorB,
+				Id:              0,
+				CoinInputsIndex: 0,
+				Items: []types.ItemRef{
+					{},
+				},
+			},
+			updateIdMsgFulfill:           false,
+			msgCreate:                    *msgCreate,
+			updateCoinInputsMsgCreate:    false,
+			updatePaymentInfosForProcess: false,
+			updateItemOutputsMsgCreate:   false,
+			setItem:                      false,
+			tradeableOfItem:              false,
+			setUpPayFeeForTradeCreate:    true,
+			setTradePercentage:           true,
 			valid:                        false,
 		},
 		{
 			desc: "Valid",
 			msgFulfill: types.MsgFulfillTrade{
-				Creator:         creator,
+				Creator:         creatorB,
 				Id:              0,
 				CoinInputsIndex: 0,
 				Items:           nil,
@@ -310,6 +324,14 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 	} {
 		suite.Run(tc.desc, func() {
 
+			tradeCreatorAddr, _ := sdk.AccAddressFromBech32(tc.msgCreate.Creator)
+			k.SetPylonsAccount(ctx, types.AccountAddr{Value: tradeCreatorAddr.String()}, types.Username{Value: tc.msgCreate.Creator})
+
+			tradeFulfillerAddr, _ := sdk.AccAddressFromBech32(tc.msgFulfill.Creator)
+			k.SetPylonsAccount(ctx, types.AccountAddr{Value: tradeFulfillerAddr.String()}, types.Username{Value: tc.msgFulfill.Creator})
+			if tc.msgCreate.ItemOutputs != nil {
+				tc.msgCreate.ItemOutputs = nil
+			}
 			//Begin config
 			if tc.updateIdMsgFulfill {
 				tc.msgFulfill.Id = math.MaxUint64
@@ -326,6 +348,23 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 				tc.msgCreate.CoinInputs = nil
 			}
 
+			if tc.updateItemOutputsMsgCreate {
+				tc.msgCreate.ItemOutputs = []types.ItemRef{
+					{
+						CookbookId: fmt.Sprintf("%d", index),
+						ItemId:     fmt.Sprintf("%d", index),
+					},
+				}
+				item := &types.Item{
+					Owner:      creatorA,
+					CookbookId: fmt.Sprintf("%d", index),
+					Id:         fmt.Sprintf("%d", index),
+					Tradeable:  true,
+				}
+
+				k.SetItem(ctx, *item)
+			}
+
 			if tc.setItem {
 
 				tc.msgFulfill.Items[0].CookbookId = fmt.Sprintf("%d", index)
@@ -337,7 +376,7 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 					},
 				}
 				item := &types.Item{
-					Owner:      creator,
+					Owner:      creatorB,
 					CookbookId: fmt.Sprintf("%d", index),
 					Id:         fmt.Sprintf("%d", index),
 				}
@@ -347,37 +386,159 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 					item.Tradeable = false
 				}
 				k.SetItem(ctx, *item)
-			} else {
-				tc.msgCreate.ItemOutputs = nil
 			}
 
-			if tc.setUpPayFee {
-				requesterAddr, _ := sdk.AccAddressFromBech32(tc.msgFulfill.Creator)
-				err := k.MintCoinsToAddr(ctx, requesterAddr, sdk.NewCoins(sdk.NewCoin("coin0", sdk.NewInt(10))))
+			if tc.setUpPayFeeForTradeFulFill {
+				err := k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin0", sdk.NewInt(4))))
 				require.NoError(err)
-				err = k.MintCoinsToAddr(ctx, requesterAddr, sdk.NewCoins(sdk.NewCoin("coin1", sdk.NewInt(10))))
+				err = k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin1", sdk.NewInt(10))))
 				require.NoError(err)
 
 				tc.msgFulfill.Items[0].CookbookId = fmt.Sprintf("%d", index)
 				tc.msgFulfill.Items[0].ItemId = fmt.Sprintf("%d", index)
-
 				tc.msgCreate.ItemInputs = []types.ItemInput{
 					{
 						Id: fmt.Sprintf("%d", index),
 					},
 				}
 				item := &types.Item{
-					Owner:           creator,
-					CookbookId:      fmt.Sprintf("%d", index),
-					Id:              fmt.Sprintf("%d", index),
-					TransferFee:     sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(10)), sdk.NewCoin("coin1", sdk.NewInt(10))},
-					TradePercentage: sdk.OneDec(),
-					Tradeable:       true,
+					Owner:       creatorB,
+					CookbookId:  fmt.Sprintf("%d", index),
+					Id:          fmt.Sprintf("%d", index),
+					TransferFee: sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(10)), sdk.NewCoin("coin1", sdk.NewInt(10))},
+					Tradeable:   true,
 				}
 				tc.msgCreate.CoinOutputs = sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(4)), sdk.NewCoin("coin1", sdk.NewInt(10))}
+				if tc.setTradePercentage {
+					item.TradePercentage = sdk.OneDec()
+				}
 				k.SetItem(ctx, *item)
-			} else {
-				tc.msgCreate.ItemOutputs = nil
+			}
+
+			if tc.setUpSendCoinsToCookBookCreator {
+				err := k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin0", sdk.NewInt(10))))
+				require.NoError(err)
+				err = k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin2", sdk.NewInt(4))))
+				require.NoError(err)
+				err = k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin4", sdk.NewInt(5))))
+				require.NoError(err)
+
+				err = k.MintCoinsToAddr(ctx, tradeFulfillerAddr, sdk.NewCoins(sdk.NewCoin("coin0", sdk.NewInt(1))))
+				require.NoError(err)
+
+				tc.msgFulfill.Items[0].CookbookId = fmt.Sprintf("%d", index)
+				tc.msgFulfill.Items[0].ItemId = fmt.Sprintf("%d", index)
+
+				tc.msgFulfill.Items[1].CookbookId = fmt.Sprintf("%d", index) + "test"
+				tc.msgFulfill.Items[1].ItemId = fmt.Sprintf("%d", index) + "test"
+
+				cookbook := &types.MsgCreateCookbook{
+					Creator:      creatorA,
+					Id:           fmt.Sprintf("%d", index),
+					Name:         fmt.Sprintf("%d", index),
+					Description:  "descdescdescdescdescdesc",
+					Developer:    "",
+					Version:      "v0.0.1",
+					SupportEmail: "test@email.com",
+					Enabled:      true,
+				}
+				_, err = srv.CreateCookbook(ctx, cookbook)
+				require.NoError(err)
+
+				cookbook2 := &types.MsgCreateCookbook{
+					Creator:      creatorA,
+					Id:           fmt.Sprintf("%d", index) + "test",
+					Name:         fmt.Sprintf("%d", index) + "test",
+					Description:  "descdescdescdescdescdesc",
+					Developer:    "",
+					Version:      "v0.0.1",
+					SupportEmail: "test@email.com",
+					Enabled:      true,
+				}
+				_, err = srv.CreateCookbook(ctx, cookbook2)
+				require.NoError(err)
+				tc.msgCreate.ItemInputs = []types.ItemInput{
+					{
+						Id: fmt.Sprintf("%d", index),
+					},
+					{
+						Id: fmt.Sprintf("%d", index) + "test",
+					},
+				}
+
+				items := []types.Item{
+					{
+						Owner:           creatorB,
+						CookbookId:      fmt.Sprintf("%d", index),
+						Id:              fmt.Sprintf("%d", index),
+						TransferFee:     sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(10)), sdk.NewCoin("coin1", sdk.NewInt(10)), sdk.NewCoin("coin2", sdk.NewInt(10))},
+						TradePercentage: sdk.OneDec(),
+						Tradeable:       true,
+					},
+					{
+						Owner:           creatorB,
+						CookbookId:      fmt.Sprintf("%d", index) + "test",
+						Id:              fmt.Sprintf("%d", index) + "test",
+						TransferFee:     sdk.Coins{sdk.NewCoin("coin2", sdk.NewInt(4)), sdk.NewCoin("coin3", sdk.NewInt(5)), sdk.NewCoin("coin4", sdk.NewInt(6))},
+						TradePercentage: sdk.OneDec(),
+						Tradeable:       true,
+					},
+				}
+				tc.msgCreate.CoinOutputs = sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(10)), sdk.NewCoin("coin2", sdk.NewInt(4)), sdk.NewCoin("coin4", sdk.NewInt(5))}
+
+				for _, item := range items {
+					k.SetItem(ctx, item)
+				}
+			}
+
+			if tc.setUpPayFeeForTradeCreate {
+				err := k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin0", sdk.NewInt(4))))
+				require.NoError(err)
+				err = k.MintCoinsToAddr(ctx, tradeCreatorAddr, sdk.NewCoins(sdk.NewCoin("coin1", sdk.NewInt(10))))
+				require.NoError(err)
+
+				err = k.MintCoinsToAddr(ctx, tradeFulfillerAddr, sdk.NewCoins(sdk.NewCoin("coin1", sdk.NewInt(10))))
+				require.NoError(err)
+
+				cookbook := &types.MsgCreateCookbook{
+					Creator:      creatorA,
+					Id:           fmt.Sprintf("%d", index),
+					Name:         fmt.Sprintf("%d", index),
+					Description:  "descdescdescdescdescdesc",
+					Developer:    "",
+					Version:      "v0.0.1",
+					SupportEmail: "test@email.com",
+					Enabled:      true,
+				}
+				_, err = srv.CreateCookbook(ctx, cookbook)
+				require.NoError(err)
+
+				tc.msgFulfill.Items[0].CookbookId = fmt.Sprintf("%d", index)
+				tc.msgFulfill.Items[0].ItemId = fmt.Sprintf("%d", index)
+				tc.msgCreate.ItemInputs = []types.ItemInput{
+					{
+						Id: fmt.Sprintf("%d", index),
+					},
+				}
+				item := &types.Item{
+					Owner:       creatorB,
+					CookbookId:  fmt.Sprintf("%d", index),
+					Id:          fmt.Sprintf("%d", index),
+					TransferFee: sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(10)), sdk.NewCoin("coin1", sdk.NewInt(10))},
+					Tradeable:   true,
+				}
+				tc.msgCreate.ItemOutputs = []types.ItemRef{
+					{
+						CookbookId: fmt.Sprintf("%d", index),
+						ItemId:     fmt.Sprintf("%d", index),
+					},
+				}
+
+				tc.msgCreate.CoinOutputs = sdk.Coins{sdk.NewCoin("coin0", sdk.NewInt(4)), sdk.NewCoin("coin1", sdk.NewInt(10))}
+				if tc.setTradePercentage {
+					item.TradePercentage = sdk.OneDec()
+				}
+				k.SetItem(ctx, *item)
 			}
 
 			if tc.updatePaymentInfosForProcess {
@@ -391,25 +552,6 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 				})
 				k.SetParams(suite.ctx, params)
 			}
-
-			if tc.updateItemOutputsMsgCreate {
-				tc.msgCreate.ItemOutputs = []types.ItemRef{
-					{
-						CookbookId: fmt.Sprintf("%d", index),
-						ItemId:     fmt.Sprintf("%d", index),
-					},
-				}
-				item := &types.Item{
-					Owner:      creator,
-					CookbookId: fmt.Sprintf("%d", index),
-					Id:         fmt.Sprintf("%d", index),
-					Tradeable:  true,
-				}
-
-				k.SetItem(ctx, *item)
-			} else {
-				tc.msgCreate.ItemOutputs = nil
-			}
 			//End config
 
 			respCreate, err := srv.CreateTrade(wctx, &tc.msgCreate)
@@ -421,6 +563,12 @@ func (suite *IntegrationTestSuite) TestFulfillTradeMsgServerSimple2() {
 				require.NoError(err)
 			} else {
 				require.Error(err)
+			}
+			if bk.SpendableCoins(ctx, tradeCreatorAddr) != nil {
+				suite.pylonsApp.BankKeeper.SendCoins(ctx, tradeCreatorAddr, trashAddress, bk.SpendableCoins(ctx, tradeCreatorAddr))
+			}
+			if bk.SpendableCoins(ctx, tradeFulfillerAddr) != nil {
+				suite.pylonsApp.BankKeeper.SendCoins(ctx, tradeFulfillerAddr, trashAddress, bk.SpendableCoins(ctx, tradeFulfillerAddr))
 			}
 		})
 	}
